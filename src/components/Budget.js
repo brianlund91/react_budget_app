c49e1aef9b050823bfa713dd65a23b289d6716a1
Brian Lund <blund05@msn.com>
2023-09-13 16:12:11 -0600

    add Budget component

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,27 @@
+import React, {useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+
+    const handleBudgetChange = (event) => {
+        setNewBudget(event.target.value);
+    };
+
+    // TODO: make hardcoded currency symbol read from state (context)?
+    return(
+        <div className='alert alert-secondary'>
+            <span>
+                Budget: £{budget} 
+            </span>
+            <input 
+                type='number' 
+                step='10' 
+                value={newBudget} 
+                onChange={handleBudgetChange}/>
+        </div>
+    );
+};
+
+export default Budget;
