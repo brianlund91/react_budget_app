48d4ce89abca7de70a7ff79e21aadaedd12c7722
Brian Lund <blund05@msn.com>
2023-09-13 17:20:00 -0600

    add a few expense components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-//Code to import Budget.js
 import Budget from './components/Budget';
-
-// Add code to import the other components here under
-
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
 
 import { AppProvider } from './context/AppContext';
+
 const App = () => {
     return (
         <AppProvider>
@@ -18,17 +17,12 @@ const App = () => {
                         <div className='col-sm'>
                             <Budget/> 
                         </div>
-
-                        {/* Add Remaining component here under */}        
-
-                        {/* Add ExpenseTotal component here under */}        
-                       
-                        {/* Add ExpenseList component here under */}         
-
-                        {/* Add ExpenseItem component here under */}        
-
-                        {/* Add AllocationForm component here under */}        
-
+                        <div className='col-sm'>
+                            <Remaining/> 
+                        </div>
+                        <div className='col-sm'>
+                            <ExpenseTotal/> 
+                        </div>      
                 </div>
             </div>
         </AppProvider>
--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,1 +1,24 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Remaining = () => {
+    const { expenses, budget } = useContext(AppContext)
+
+    const remainingBudget = budget - expenses.reduce(
+        (total, expense) => {
+            return (total + expense.cost);
+        }, 
+        0
+    );
+
+    // bad news if budget is in the negative
+    const alertType = remainingBudget < 0 ? 'alert-danger' : 'alert-success' 
+
+    return (
+        <div className={`alert ${alertType}`}>
+            Remaining Budget = £{remainingBudget}
+        </div>
+    );
+};
+
+export default Remaining;
